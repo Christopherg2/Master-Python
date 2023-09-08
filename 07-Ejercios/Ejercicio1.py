b54abc7119a4938ed6356668c438d9b6d9a1b1e9
Christopher Garcia <imchristopherg2@gmail.com>
2023-09-08 17:51:10 -0600

    Ejercios
    Ejercios

---
+++ 07-Ejercios/Ejercicio1.py
@@ -0,0 +1,7 @@
+pais = "México"  # Tipo de dato str
+continente = "America Central "  # Tipo str
+año = 2023  # Tipo int
+
+print(
+    f"Hola Soy chris y soy de {pais} que pertenece a {continente} es el año {str(año)} ")
+# Se une las variables en el print con "f", además con str se convierte la variable int en str.
---
+++ 07-Ejercios/Ejercicio2.py
@@ -0,0 +1,17 @@
+# Script que muestre por pantalla todos los números pares del 1 al 120.
+
+# contador = 1  # Creamos la variable y la inicializamos con el valor 1
+# Tambien mencionar que en python, se declar y inializar al principio, sino hasta el ciclo
+
+for contador in range(1, 121):  # eL Contador se iniziliza, se crea un rango para los
+    # numeros pales
+
+    if contador % 2 == 0:
+        print(contador)
+    else:
+        print(f"{contador} Es impar")
+"""_
+ el for recorre cada numero del rango, cada numero recorrido lo guarda en la variable
+ contador, el numero se divide entre dos y si el resultado es igual igual a 0 s
+ se muestra, esa es la formula para saber si es par. 
+ """
