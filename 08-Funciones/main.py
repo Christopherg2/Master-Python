ce9fc11d956ee1c77b4dea110681c140b9f6f2b1
Christopher Garcia <imchristopherg2@gmail.com>
2023-09-09 21:33:08 -0600

    Funciones en Python
    Ejercicios con funciones, ciclos, variabales.

---
+++ 08-Funciones/main.py
@@ -0,0 +1,72 @@
+"""
+Funciones: 
+Una función es es un conjunto de intrucciones agrupadas bajo
+un nombre concreto que pueden reutilizarse invocando 
+a la función tantas veces como sea necesario.
+
+def nombreDeMiFunción(parametros):
+   #BLOQUE / CONJUNTO DE INSTRUCCIONES
+
+nombreDeMiFuncion(mi_parametro)
+nombreDeMiFuncion(mi_parametro)
+ 
+"""
+"""
+# Ejemplo 1
+print("EJEMPLO 1")
+
+
+def muestra_nombres():
+    print("Chris")
+    print("Dylan")
+    print("Silvia")
+    print("\n")
+
+
+# Invocar funcion
+muestra_nombres()
+"""
+"""
+# Ejemplo 2
+print(" ### EJEMPLO 2 ### ")
+
+
+def mostrarnombre(nombre, edad):
+    print(f"Tu nombre es: {nombre}")
+    if edad >= 18:
+        print("Y eres mayor de edad")
+    else:
+        print("Y  NO eres mayor de edad")
+
+
+nombre = input("Introduce tu nombre: ")
+edad = int(input("Introduce tu edad: "))
+
+mostrarnombre(nombre, edad)
+# mostrarnombre("Dylan")
+# mostrarnombre("Silvia")
+"""
+
+# Ejemplo 3
+print(" ### EJEMPLO 3 ### ")
+
+
+def tabla(numero):
+    print(f"Tabla de multiplicar del número: {numero}")
+    for contador in range(11):
+        operación = numero*contador
+        print(f"{numero} x {contador} = {operación }")
+
+    print("\n")
+
+
+tabla(3)
+tabla(8)
+tabla(9)
+
+# Ejemplo 3.1
+print(" ---------------- ")
+print(" ### EJEMPLO 3.1 ### ")
+
+for numero_tabla in range(1, 11):
+    tabla(numero_tabla)
