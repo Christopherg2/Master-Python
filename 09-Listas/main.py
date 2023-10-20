a4e1ea5cc355db8301ce3b8e5f855e633c14f398
Christopher Garcia <imchristopherg2@gmail.com>
2023-10-20 11:48:06 -0600

    Listas.py
    Ejemplo de listas

---
+++ 09-Listas/main.py
@@ -0,0 +1,25 @@
+""""
+Lista (arrays)
+Son coleciones o conjuntos de datos/valores, bajoun unico nombre.
+Para aceder a esos valores podemos usar un indice numerico
+"""
+pelicula = "Batman"
+
+# definir lista
+peliculas = ["Batman", "Superman", "Robin"]
+cantantes = list(('Zoe', 'Clairo', 'TOP'))
+# doble (()) dupla para que no marque error
+year = list(range(2020, 2060))
+variada = ["Christopher", 80, 8.8, True, "Textooooxs"]
+
+"""
+print(peliculas)
+print(cantantes)
+print(variada)
+"""
+
+# INDICES
+print(peliculas[1])
+print(cantantes[1])
+print(year)
+print(variada[0])
